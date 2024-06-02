de78280f2dcf3798e3e731143ed1ee6b21f30175
Francisco <fruiz@fi.uba.ar>
2024-06-02 19:58:04 -0300

    add db connection and run-model endpoint

--- app.py
+++ app.py
@@ -1,31 +1,6 @@
-from flask import Flask, jsonify
-import numpy as np
+from app import create_app
 
-app = Flask("adsolab")
-
-
-@app.route('/health-check', methods=['GET'])
-def health_check():
-    return jsonify({"status": "ok"})
-
-
-def apply_langmuir(ce):
-    return 0.198 * (0.189 * ce) / (1 + 0.189 * ce)
-
-
-@app.route('/langmuir', methods=['GET'])
-def test():
-    ce = np.array([4.7, 7.0, 9.31, 16.6, 32.5, 62.8])
-    apply_model = np.vectorize(apply_langmuir)
-    results = list(apply_model(ce))
-    return jsonify(results), 200
-
-
-@app.after_request
-def add_header(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
---
+++ app/routes.py
@@ -0,0 +1,51 @@
+from flask import jsonify, request, Blueprint
+import numpy as np
+from app.entities.model import Model
+
+app = Blueprint('app', __name__)
+
+@app.route('/health-check', methods=['GET'])
+def health_check():
+    return jsonify({"status": "ok"})
+
+
+def apply_langmuir(ce):
+    return 0.198 * (0.189 * ce) / (1 + 0.189 * ce)
+
+@app.route('/run-model/<model_name>', methods=['POST'])
+def run_model(model_name):
+    data = request.get_json()
+    dot_x = data['x']
+    x = np.array(dot_x)
+
+    apply_model = np.vectorize(apply_langmuir)
+    y_model = list(apply_model(x))
+
+    model = Model.query.filter_by(name=model_name).first()
+    if not model:
+        return jsonify({"status": "error", "message": "Model not found"}), 404
+
+    response = {
+        "x": dot_x,
+        "y": y_model,
+        "formula": model.formula
+    }
+
+    return jsonify(response), 200
+
+@app.route('/models', methods=['GET'])
+def get_models():
+    models = Model.query.all()
+    if not models:
+        return jsonify({"status": "error", "message": "No models found"}), 404
+    output = []
+    for model in models:
+        model_json = {'id': model.id, 'name': model.name, 'formula': model.formula}
+        output.append(model_json)
+    response = {'models': output}
+    return jsonify(response),200
+
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
