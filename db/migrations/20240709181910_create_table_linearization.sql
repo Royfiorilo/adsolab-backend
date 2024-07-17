365ed41bba6aecd57d2ec6c2cd57fb8b94a1a5ac
francisco <fruiz@fi.uba.ar>
2024-07-17 19:39:00 -0300

    Modify linearization_table migration

--- db/migrations/20240709181910_create_table_linearization.sql
+++ db/migrations/20240709181910_create_table_linearization.sql
@@ -8,7 +8,7 @@ create table linearization
     description      varchar(500) not null,
     parameters       json         not null,
     model_id         integer      not null
-        references model
+        references model(_id)
 );
 
 -- migrate:down
