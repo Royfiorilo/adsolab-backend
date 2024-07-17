68831ad36618fdc4f6ff30a866944150cd01d119
francisco <fruiz@fi.uba.ar>
2024-07-17 19:34:49 -0300

    Modify linearization_table migration

--- db/migrations/20240709181910_create_table_linearization.sql
+++ db/migrations/20240709181910_create_table_linearization.sql
@@ -1,12 +1,14 @@
 -- migrate:up
 create table linearization
 (
-    linearization_id serial primary key,
+    linearization_id serial
+        primary key,
     name             varchar(100) not null,
     formula          varchar(255) not null,
     description      varchar(500) not null,
     parameters       json         not null,
-    model_id         INTEGER REFERENCES model(id) ON DELETE CASCADE
+    model_id         integer      not null
+        references model
 );
 
 -- migrate:down
