d2746e623a57cdb452e69098a7b134cd4f70895f
agosvasquez <maria.vasquez@despegar.com>
2025-01-11 16:41:06 -0300

    logging and delete default step

--- app/controler/investigation_controller.py
+++ app/controler/investigation_controller.py
@@ -55,9 +55,9 @@ def execute_no_linear_models():
     if "investigation_id" not in request_json or "models" not in request_json:
         raise BadRequestError("investigation_id and models are required")
 
-    logging.log(f"Arranco a comparar:{datetime.now()}")
+    logging.info(f"Arranco a comparar:{datetime.now()}")
     results, comparision = run_no_linear_models(request_json)
-    logging.log(f"Finaliza la comparacion:{datetime.now()}")
+    logging.info(f"Finaliza la comparacion:{datetime.now()}")
     response = {
         "investigation_id": request_json['investigation_id'],
         "results": results,
