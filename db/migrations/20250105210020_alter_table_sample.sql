5a91de6dd047625f54b8050ce0cb6f3a810a2682
FRuiz811 <58398663+FRuiz811@users.noreply.github.com>
2025-01-22 20:20:25 -0300

    Update 20250105210020_alter_table_sample.sql

--- db/migrations/20250105210020_alter_table_sample.sql
+++ db/migrations/20250105210020_alter_table_sample.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 ALTER TABLE sample
-ADD COLUMN adsorbate_id integer not null references adsorbate,
-ADD COLUMN adsorbent_id integer not null references adsorbent,
+ADD COLUMN adsorbate_id integer not null default 1 references adsorbate,
+ADD COLUMN adsorbent_id integer not null default 1 references adsorbent,
 ADD COLUMN temperature double precision,
 ADD COLUMN measure_unit varchar(10);
 
