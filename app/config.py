47827db6de1d4f781feda1d0ff9dce2644a77194
Federico Rossini <federico.rossini@kavak.com>
2025-03-16 20:17:38 -0300

    fix: remove security config

--- app/config.py
+++ app/config.py
@@ -19,12 +19,7 @@ class Config:
     WTF_CSRF_CHECK_DEFAULT = False
     SECURITY_CSRF_PROTECT_MECHANISMS = ["session"]
     SECURITY_CSRF_COOKIE_NAME = "XSRF-TOKEN"
-    SECURITY_CSRF_COOKIE = {"samesite": "None", "httponly": False, "secure": True}
     WTF_CSRF_TIME_LIMIT = None
-    REMEMBER_COOKIE_SAMESITE = "None"
-    SESSION_COOKIE_SAMESITE = "None"
-    SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_DOMAIN = os.getenv('DOMAIN', 'localhost')
     # --- Flask Security ---
 
     SQLALCHEMY_ENGINE_OPTIONS = {
